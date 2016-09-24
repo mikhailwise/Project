1bb5702b31e1791231887cf119ee8c16296fedb4
--global <--global>
2016-09-23 22:41:10 -0400

    Just updating the file.

--- learn.py
+++ learn.py
@@ -1,8 +1,9 @@
 from sys import argv
 
-script, filename = argv
+script, user_name = argv
 prompt = '> '
-print "Hi %s I'm the %s script." (user_name, script
+
+print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
 print "Do you like me %s?" % user_name
 likes = raw_input(prompt)
@@ -17,4 +18,4 @@ print """
 Alright, so you said %r about liking me.
 You live in %r.  Not sure where that is.
 And you have a %r computer.  Nice.
-""" % (likes, lives, computer))
+""" % (likes, lives, computer)
